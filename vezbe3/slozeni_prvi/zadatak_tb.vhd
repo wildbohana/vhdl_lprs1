8faba185eebf96ab75eb16e19316339e56dd6569
wildbohana <83251325+wildbohana@users.noreply.github.com>
2021-11-22 19:24:05 +0100

    testbenč

--- vezbe3/slozeni_prvi/zadatak_tb.vhd
+++ vezbe3/slozeni_prvi/zadatak_tb.vhd
@@ -6,19 +6,19 @@ end entity;
 
 architecture Behavioral of zadatak_tb is
 	
-	signal sA : std_logic_vector(7 downto 0);
-	signal sB : std_logic_vector(2 downto 0);
-	signal sSEL : std_logic_vector(1 downto 0);
-	signal sY : std_logic_vector(3 downto 0);
-	signal sZERO : std_logic;
+	signal sA 	: std_logic_vector(7 downto 0);
+	signal sB 	: std_logic_vector(2 downto 0);
+	signal sSEL 	: std_logic_vector(1 downto 0);
+	signal sY 	: std_logic_vector(3 downto 0);
+	signal sZERO 	: std_logic;
 
 	component zadatak is
 		port(
-			iA : in std_logic_vector(7 downto 0);
-			iB : in std_logic_vector(2 downto 0);
-			iSEL : in std_logic_vector(1 downto 0);
-			oY : out std_logic_vector(3 downto 0);
-			oZERO : out std_logic
+			iA 	:  in std_logic_vector(7 downto 0);
+			iB 	:  in std_logic_vector(2 downto 0);
+			iSEL 	:  in std_logic_vector(1 downto 0);
+			oY 	: out std_logic_vector(3 downto 0);
+			oZERO 	: out std_logic
 		);
 	end component;
 
