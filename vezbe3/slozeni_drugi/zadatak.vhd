5b008d345ca681287938d1f9e8125469dba19907
wildbohana <83251325+wildbohana@users.noreply.github.com>
2021-11-22 19:19:57 +0100

    glavni deo

--- vezbe3/slozeni_drugi/zadatak.vhd
+++ vezbe3/slozeni_drugi/zadatak.vhd
@@ -5,10 +5,10 @@ use ieee.std_logic_unsigned.all;
 
 entity slozeni_sistem_2 is
 	port(
-		iA : in std_logic_vector(3 downto 0);
-		iB : in std_logic_vector(3 downto 0);
-		iSEL : in std_logic_vector(1 downto 0);
-		oC : out std_logic_vector(3 downto 0)
+		iA   :  in std_logic_vector(3 downto 0);
+		iB   :  in std_logic_vector(3 downto 0);
+		iSEL :  in std_logic_vector(1 downto 0);
+		oC   : out std_logic_vector(3 downto 0)
 	);
 end entity;
 
@@ -17,12 +17,12 @@ architecture Behavioral of slozeni_sistem_2 is
 		signal sB : std_logic_vector(3 downto 0);
 		
 begin
-	sB <= not('0' & iB(2 downto 0)) + 1 when iB(3) = '1' else
-			iB; -- pozitivan
+	sB  <=  not('0' & iB(2 downto 0)) + 1 when iB(3) = '1' else
+		iB; 	-- pozitivan
 			
-	oC <= iA			when iSEL = "00" else
-			iB			when iSEL = "01" else
-			iA + sB  when iSEL = "10" else
-			sB + 1;
+	oC <= 	iA 	when iSEL = "00" else
+		iB 	when iSEL = "01" else
+		iA + sB when iSEL = "10" else
+		sB + 1;
 
 end architecture;
