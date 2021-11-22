4decf23da3e23cfeea97ca1afb9070bc341f96f5
wildbohana <83251325+wildbohana@users.noreply.github.com>
2021-11-22 19:27:03 +0100

    testbenč

--- vezbe4/brojac/brojac_tb.vhd
+++ vezbe4/brojac/brojac_tb.vhd
@@ -1,33 +1,63 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_unsigned.ALL;
 
-entity Brojac is port ( 
-	iRST  : in  std_logic;
-	iCLK  : in  std_logic;
-	iEN   : in  std_logic;
-	oCNT  : out std_logic_vector(7 downto 0)
-	);
+entity Brojac_tb is
 end entity;
-
-architecture Behavioral of Brojac is
-
-	signal sCNT : std_logic_vector(7 downto 0);
+ 
+architecture Test of Brojac_tb is
+  
+   signal sCLK : std_logic := '0';
+   signal sRST : std_logic := '0';
+   signal sEN  : std_logic := '0';
+   signal sCNT : std_logic_vector(7 downto 0);
 	
+	constant iCLK_PERIOD : time := 10 ns;
+	
+   component Brojac is port (   
+		iRST  : in  std_logic;
+		iCLK  : in  std_logic;
+		iEN   : in  std_logic;
+		oCNT  : out std_logic_vector(7 downto 0)
+	);
+   end component;
+
 begin
+
+   uut: Brojac port map (
+          iCLK => sCLK,
+          iRST => sRST,
+          iEN => sEN,
+          oCNT => sCNT
+        );
 	
-	process (iRST, iCLK) begin						-- asinhroni
-		if(iRST = '1') then
-			sCNT <= "00000000";
-		elsif (iCLK'event and iCLK = '1') then		-- rastuća ivica
-			if(iEN = '1') then
-				if(sCNT = 82) then					-- po modulu 83
-					sCNT <= "00000000";
-				else
-					sCNT <= sCNT + 1;
-				end if;
-			end if;
-		end if;
+	--takt process
+	clk_proc : process
+	begin
+		sCLK <= '1';
+		wait for iCLK_PERIOD / 2;
+		sCLK <= '0';
+		wait for iCLK_PERIOD / 2;
 	end process;
 	
-end Behavioral;
+   stim_proc : process
+   begin
+		sRST <= '1';					-- pomeraj ulaznog signala (asinhoro)
+		wait for 3.25 * iCLK_period;
+		sRST <= '0';
+
+		sEN <= '1';					-- 15
+		wait for 15 * iCLK_period;
+
+		sEN <= '0';
+		wait for 5 * iCLK_period;			-- ovo ignorišeš jer nula
+
+		sEN <= '1';					-- plus 35 jednako 50
+		wait for 35 * iCLK_period;
+
+		sRST <= '1';
+		wait for 3 * iCLK_period;
+		sRST <= '0';
+
+		wait;
+   end process;
+end architecture;
