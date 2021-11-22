edb2ef64a663ed701895047eb35f440c87b5ba5b
wildbohana <83251325+wildbohana@users.noreply.github.com>
2021-11-22 19:28:41 +0100

    testbenč

--- vezbe4/pomReg/pomReg_tb.vhd
+++ vezbe4/pomReg/pomReg_tb.vhd
@@ -8,8 +8,8 @@ end entity;
 architecture Test of PomReg_tb is
 
 	signal sRST   :   std_logic := '0';
-   signal sCLK   :   std_logic := '0';
-   signal sLOAD  :   std_logic := '0';
+   	signal sCLK   :   std_logic := '0';
+   	signal sLOAD  :   std_logic := '0';
 	signal sARITH :   std_logic := '0';
 	signal sSHL   :   std_logic := '0';
 	signal sSHR   :   std_logic := '0';
@@ -19,15 +19,15 @@ architecture Test of PomReg_tb is
 	constant iCLK_PERIOD : time := 10 ns;
 
 component PomReg 
-   Port ( iRST   : in  std_logic;
-          iCLK   : in  std_logic;
-          iLOAD  : in  std_logic;
-			 iARITH : in  std_logic;
-			 iSHL   : in  std_logic;
-			 iSHR   : in  std_logic;
-			 iDATA  : in  std_logic_vector(7 downto 0);
-			 oSHREG : out std_logic_vector(7 downto 0)
-			 );
+   Port ( 	iRST   : in  std_logic;
+          	iCLK   : in  std_logic;
+          	iLOAD  : in  std_logic;
+		iARITH : in  std_logic;
+		iSHL   : in  std_logic;
+		iSHR   : in  std_logic;
+		iDATA  : in  std_logic_vector(7 downto 0);
+		oSHREG : out std_logic_vector(7 downto 0)
+		);
 end component;
 	
 begin
