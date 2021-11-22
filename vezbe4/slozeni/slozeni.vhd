0e1789393a45430818a0c3d04bf160ccab8bdd7d
wildbohana <83251325+wildbohana@users.noreply.github.com>
2021-11-22 19:12:58 +0100

    glavni deo

--- vezbe4/slozeni/slozeni.vhd
+++ vezbe4/slozeni/slozeni.vhd
@@ -1,70 +1,60 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
- 
-entity Slozeni_tb is
-end entity;
- 
-architecture Test of Slozeni_tb is
 
-	--Inputs and outputs
-    signal sCLK   : std_logic := '0';
-    signal sRST   : std_logic := '0';
-    signal sEN    : std_logic := '0';
-	signal sCNT   : std_logic_vector(7 downto 0);
-    signal sSHREG : std_logic_vector(7 downto 0);
-	
-	constant iCLK_period : time := 10 ns;
-	
-	component Slozeni is port(
-	iCLK   : in  std_logic;
-    iRST   : in  std_logic;
-    iEN    : in  std_logic;
-	oCNT   : out std_logic_vector (7 downto 0);
-    oSHREG : out std_logic_vector(7 downto 0)
-    );
-   end component;
+entity Slozeni is port (
+		iCLK   : in  std_logic;
+      iRST   : in  std_logic;
+      iEN    : in  std_logic;
+		oCNT   : out std_logic_vector (7 downto 0);
+      oSHREG : out  std_logic_vector (7 downto 0)
+		);
+end entity;
+
+architecture Behavioral of Slozeni is
+
+	signal sCNT : std_logic_vector(7 downto 0);
+	signal sSHREG : std_logic_vector(7 downto 0);
+	signal sLOAD : std_logic;
+	signal sARITH : std_logic;
 
 begin
 
-	uut: Slozeni port map (
-        iCLK => sCLK,
-        iRST => sRST,
-        iEN => sEN,
-		oCNT => sCNT,
-        oSHREG => sSHREG
-        );
+	process (iCLK, iRST) begin						-- asinhroni
+		if(iRST = '1') then
+			sCNT <= "00000000";
+		elsif (iCLK'event and iCLK = '1') then		-- rastuća ivica
+			if(iEN = '1') then
+				sCNT <= sCNT + 1;
+			end if;
+		end if;
+	end process;
+	-- 3 19 15
 	
-	--takt process
-	clk_proc : process
-	begin
-		sCLK <= '1';
-		wait for iCLK_period / 2;
-		sCLK <= '0';
-		wait for iCLK_period / 2;
+	-- 8-bit counter
+	process (iCLK, iRST) begin
+		if(iRST = '1') then
+			sSHREG <= "00000000";
+		elsif(rising_edge(iCLK)) then
+			if(sLOAD = '1') then
+				sSHREG <= sCNT;				-- učitaj vrednost iz brojača
+			else
+				if(sARITH = '1') then		-- aritmetički udesno
+					sSHREG <= sSHREG(7) & sSHREG(7 downto 1);
+				else
+					sSHREG <= sSHREG(6 downto 0) & '0';
+				end if;
+			end if;
+		end if;
+
 	end process;
 	
-   -- Stimulus process
-	stim_proc: process
-	begin
-		sRST <= '1';
-		wait for 2.75 * iCLK_period;
-		sRST <= '0';
+	sLOAD  <= 	'1' when (sCNT = 8 or sCNT = 128) else
+				'0';
 
-		sEN <= '1';
-		wait for 25 * iCLK_period;
+	sARITH  <= 	'1' when sCNT > 128 else
+				'0';
 
-		sEN <= '0';
-		wait for 30 * iCLK_period;
-
-		sEN <= '1';
-		wait for 50 * iCLK_period;
-
-		sRST <= '1';
-		wait for 2 * iCLK_period;
-		sRST <= '0';
-		
-		wait;
-   end process;
-	
-END;
+	oSHREG <= sSHREG;
+	oCNT <= sCNT;
+end architecture;
