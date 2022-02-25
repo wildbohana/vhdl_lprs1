dbc6e67106c7fc72c770cbe4efb554e4ee6077ce
wildbohana <83251325+wildbohana@users.noreply.github.com>
2022-02-25 06:12:09 +0100

    za ispit

--- primeri/02 sekvencijalna kola.vhd
+++ primeri/02 sekvencijalna kola.vhd
@@ -17,48 +17,47 @@ end entity;
 
 architecture arch of kolo is
 	-- Constants.
-	-- za brojač - mod25 ide do 24 (11000)
+	-- mod25 ide do 24 (11000)
 	constant cMod25	: std_logic_vector(4 downto 0) := "11000";
 
 	-- Signals.
-	signal s_en_1us	: std_logic;
-	signal s_tc_1us : std_logic;
-	signal s_cnt_1us: std_logic_vector(4 downto 0);
-	signal s_en0 	: std_logic;
-	signal s_tc0 	: std_logic;
-	signal s_cnt0 	: std_logic_vector(3 downto 0) := "0000";
-	signal s_en1 	: std_logic;
-	signal s_cnt1	: std_logic_vector(3 downto 0) := "0000";
-	signal s_tc1 	: std_logic;
+	signal s_en_1us	 : std_logic;
+	signal s_tc_1us  : std_logic;
+	signal s_cnt_1us : std_logic_vector(4 downto 0);
+	signal s_en0 	 : std_logic;
+	signal s_tc0 	 : std_logic;
+	signal s_cnt0 	 : std_logic_vector(3 downto 0) := "0000";
+	signal s_en1 	 : std_logic;
+	signal s_cnt1	 : std_logic_vector(3 downto 0) := "0000";
+	signal s_tc1 	 : std_logic;
 
 --------------------------------------------------------------------------------
 
 begin
-
 	-- Kontrola dozvole brojanja, asinhroni reset
-	process(i_clk, i_rst) begin
-		if(i_rst = '1') then
+	process (i_clk, i_rst) begin
+		if (i_rst = '1') then
 			s_en <= '0';
-		elsif(i_clk'event and i_clk = '1') then
-			if(i_rst = '1') then
+		elsif (i_clk'event and i_clk = '1') then
+			if (i_rst = '1') then
 				s_en <= '0';
-			elsif(i_run = '1') then
+			elsif (i_run = '1') then
 				s_en <= '1';
-			elsif(i_pause = '1') then
+			elsif (i_pause = '1') then
 				s_en <= '0';
 			end if;
 		end if;
 	end process;
 	
 	-- Brojač po mod25, asinhroni reset
-	process(i_clk, i_rst) begin
-		if(i_rst = '1') then
+	process (i_clk, i_rst) begin
+		if (i_rst = '1') then
 			s_cnt <= "00000";
-		elsif(rising_edge(i_clk)) then
-			if(s_en = '1') then					-- dozvola brojanja
-				if(s_cnt < cMod25) then
+		elsif (rising_edge(i_clk)) then
+			if (s_en = '1') then			-- dozvola brojanja
+				if (s_cnt < cMod25) then
 					s_cnt <= s_cnt + 1;
-				elsif(s_cnt >= cMod25) then
+				elsif (s_cnt >= cMod25) then
 					s_cnt <= "00000";
 				end if;
 			end if;
@@ -67,9 +66,9 @@ begin
 	end process;
 
 	-- Registar za pamćenje, sinhroni reset
-	process(i_clk) begin
-		if(falling_edge(i_clk)) then
-			if(i_rst = '1') then
+	process (i_clk) begin
+		if (falling_edge(i_clk)) then
+			if (i_rst = '1') then
 				s_cnt_temp <= "00000";
 			else
 				s_cnt_temp <= s_cnt;
@@ -80,27 +79,25 @@ begin
 
 	-- POMERAČKI REGISTAR --
 	process (i_rst, i_clk) begin
-		if(i_rst = '1') then
+		if (i_rst = '1') then
 			s_shreg <= "00000000";
-		elsif(rising_edge(iCLK)) is
-			if(i_load = '1') then
+		elsif (rising_edge(iCLK)) is
+			if (i_load = '1') then
 				s_shreg <= s_cnt_temp;
-			elsif(i_shr = '1' and i_shl = '0') then
-				if(i_artih = '1') then				-- aritm. udesno
+			elsif (i_shr = '1' and i_shl = '0') then
+				if (i_artih = '1') then		-- aritm. udesno
 					s_shreg <= s_shreg(7) & s_shreg(7 downto 1);
-				else								-- log. udesno
+				else				-- log. udesno
 					s_shreg <= '0' & s_shreg(7 downto 1);
 				end if;
-			elsif(i_shl = '1' and i_shr = '0') then
-				if(i_artih = '1') then				-- aritm. ulevo
+			elsif (i_shl = '1' and i_shr = '0') then
+				if (i_artih = '1') then		-- aritm. ulevo
 					s_shreg <= s_shreg(6 downto 0) & '0';
-				else								-- log. ulevo
+				else				-- log. ulevo
 					s_shreg <= s_shreg(6 downto 0) & '0';
 				end if;
 			end if;
 		end if;
 	end process;	
-
-
 
 end architecture;
